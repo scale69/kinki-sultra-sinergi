049174bcdec2a9711fd675874cfbca4a662b5874
lawtheoscar@gmial.com <lawtheoscar@gmail.com>
2026-02-22 01:38:06 +0900

    change metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NextIntlClientProvider } from 'next-intl';
+import { getTranslations } from "next-intl/server";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -12,16 +13,31 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: 'Kinki Sultra Sinergi (KSS) - Jembatan Karier Indonesia-Jepang',
-  description: 'KSS adalah Kantor Sanding Organisasi (SO) di Jepang yang membantu tenaga kerja Indonesia meraih karier yang aman, legal, dan berkelanjutan di Jepang.',
-  keywords: 'tenaga kerja indonesia jepang, magang jepang, tokutei ginou, kerja di jepang, SO jepang',
-  openGraph: {
-    title: 'Kinki Sultra Sinergi (KSS) - Jembatan Karier Indonesia-Jepang',
-    description: 'Mitra profesional tenaga kerja Indonesia di Jepang. Transparan, Legal, Terpercaya.',
-    type: 'website',
-  },
-};
+// export const metadata: Metadata = {
+//   title: 'Kinki Sultra Sinergi (KSS) - Jembatan Karier Indonesia-Jepang',
+//   description: 'KSS adalah Kantor Sanding Organisasi (SO) di Jepang yang membantu tenaga kerja Indonesia meraih karier yang aman, legal, dan berkelanjutan di Jepang.',
+//   keywords: 'tenaga kerja indonesia jepang, magang jepang, tokutei ginou, kerja di jepang, SO jepang',
+//   openGraph: {
+//     title: 'Kinki Sultra Sinergi (KSS) - Jembatan Karier Indonesia-Jepang',
+//     description: 'Mitra profesional tenaga kerja Indonesia di Jepang. Transparan, Legal, Terpercaya.',
+//     type: 'website',
+//   },
+// };
+
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations("data.MetaData");
+
+  return {
+    title: t("title"),
+    description: t("description"),
+    keywords: t("keywords"),
+    openGraph: {
+      title: t("openGraph.title"),
+      description: t("openGraph.description"),
+      type: "website",
+    },
+  };
+}
 
 export default function RootLayout({
   children,
